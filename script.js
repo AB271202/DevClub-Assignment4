a5ff75db2d5289be75ab7bb78cee96ef6e012d8e
AB271202 <anishbanerjee2002@gmail.com>
2022-03-22 23:13:45 +0530

    Added cases and chart to Covid Guide

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+/**
+function myfunc(num,element,step){
+    const myInt=setInterval(function() {element.innerText=String(Number(element.innerText)+step)},1);
+    if (Number(element.innerText)>=num) {
+        clearInterval(myInt); 
+        element.innerText=num
+    }
+}
+*/
 console.log("Loaded script.js");
+
+let req= new XMLHttpRequest();
+req.open("GET","https://api.covid19api.com/summary",false)
+req.send()
+
+let data1= JSON.parse(req.response)
+let India = data1.Countries['77']
+
+//summary of total cases and cases today Globally and in India
+let gTotC=Number(data1.Global.TotalConfirmed);
+let gNewC=data1.Global.NewConfirmed;
+let iTotC=India.TotalConfirmed;
+let iNewC=India.NewConfirmed;
+
+//myfunc(10,document.getElementById("gtc"),2)
+
+document.getElementById("gtc").innerText=gTotC;
+document.getElementById("gnc").innerText=gNewC;
+document.getElementById("itc").innerText=iTotC;
+document.getElementById("inc").innerText=iNewC;
---
+++ script2.js
@@ -0,0 +1,34 @@
+let date= new Date()
+let begin =String(date.getFullYear())+"-"+String(date.getMonth())+"-"+String(date.getDate())
+let end= String(date.getFullYear())+"-"+String(date.getMonth()+1)+"-"+String(date.getDate())
+
+let req2= new XMLHttpRequest();
+req2.open("GET",`https://api.covid19api.com/country/india/status/confirmed?from=${begin}T00:00:00Z&to=${end}T00:00:00Z`,false)
+req2.send()
+
+let month=JSON.parse(req2.response)
+console.log(month)
+let cases=[], dates=[]
+for (let i=0; i<month.length; i++){
+    cases.push(month[i].Cases)
+    dates.push(month[i].Date.slice(0,-10))
+}
+console.log(cases,dates)
+
+const labels = dates
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'COVID Cases in India In the last few days',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: cases
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
